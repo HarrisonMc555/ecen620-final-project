ddb2fccc607b16f5b12d3fe8bc6d09da049b6d87
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-19 13:36:57 -0600

    Add Transaction.sv

---
+++ src/test/Transaction.sv
@@ -0,0 +1,13 @@
+class Transaction;
+
+   rand logic[15:0] instruction;
+   
+   function new(logic[15:0] instruction=16'h00);
+      this.instruction = instruction;
+   endfunction
+
+   virtual function Transaction copy();
+      copy = new(instruction);
+   endfunction
+
+endclass
