d41fae97df1eb2e4b9500c614d4ae8db65486ab6
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-22 17:01:09 -0600

    Remove unused test instantiations

--- src/test/test.sv
+++ src/test/test.sv
@@ -4,9 +4,6 @@ program automatic test(dut_if.Memory dut_if);
 
    initial begin
       // List each test
-      // static TestGood TestGood_handle = new();
-      // static Test_v3 Test_v3_handle = new();
-      // static TestBad TestBad_handle = new();
       static TestRandomGood trg = new(dut_if);
       static TestWithBad twb = new(dut_if);
       tb = TestRegistry::get_test();
