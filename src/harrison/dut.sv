b7618ad1ff0c35584f12a7f020011151cbc6d44c
thunder-hammer <dmsisawesome@gmail.com>
2019-04-22 12:55:51 -0600

    added some signals to allow for proper connection

--- src/harrison/dut.sv
+++ src/harrison/dut.sv
@@ -1,3 +1,6 @@
+
+import lc3::*;
+
 module dut
   (input bit         clk, reset,
    input bit [15:0]  dataFromMemory,
@@ -12,6 +15,7 @@ module dut
           flagWE;
    logic [1:0] selPC, selEAB2;
    logic [2:0] DR, SR1, SR2;
+   aluControl_t aluControl;
 
    datapath datapath(.*);
    controller controller(.*);
