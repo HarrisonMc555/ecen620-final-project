110635334c34f34fb560177cc81ca8c6eb9f4d71
thunder-hammer <dmsisawesome@gmail.com>
2019-04-22 14:08:35 -0600

    added the clock to the design

--- src/test/top.sv
+++ src/test/top.sv
@@ -3,6 +3,14 @@ module top;
    bit clk, rst_n;
    dut_if dut_if();
 
+   initial begin
+   clk <= 0;
+   //period of 100ns;
+   forever begin
+     #50ns clk <= ~clk;
+   end
+   end 
+
    clock_unit clock(clk);
    test test(dut_if);
    dut dut(.clk(dut_if.clk),
