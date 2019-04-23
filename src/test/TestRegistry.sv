539e26b087f3ffd97ca44696e3895e91199ac6cd
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-22 22:38:17 -0600

    Add all sources callback

--- src/test/TestRegistry.sv
+++ src/test/TestRegistry.sv
@@ -33,21 +33,28 @@ endclass
 import opcodes::*;
 
 class AllOpcodesCb extends Checker_cbs;
-   Transaction cur_tr;
+   logic [3:0] opcode;
    const string NAME = "All opcodes";
 
    covergroup cg;
-      coverpoint cur_tr.instruction[15:12] {
-         bins valid_opcodes[] = {ADD,
+      coverpoint opcode {
+      // coverpoint cur_tr.instruction[15:12] {
+         bins valid_opcodes[] = {
+                                 ADD,
                                  AND,
                                  NOT,
-                                 JSR,
                                  BR,
+                                 JMP,
+                                 JSR,
                                  LD,
+                                 LDI,
+                                 LDR,
+                                 LEA,
                                  ST,
-                                 STR,
                                  STI,
-                                 JMP};
+                                 STR,
+                                 TRAP
+                                 };
       }
    endgroup
 
@@ -57,7 +64,66 @@ class AllOpcodesCb extends Checker_cbs;
    endfunction
 
    task post_tx(ref Transaction tr);
-      cur_tr = tr;
+      opcode = tr.instruction[15:12];
+      cg.sample();
+   endtask
+
+   virtual function logic is_done();
+      return cg.get_coverage() == 100.0;
+   endfunction
+
+   virtual function real get_coverage;
+      return cg.get_coverage();
+   endfunction
+
+   virtual function string coverage_name();
+      return NAME;
+   endfunction
+endclass
+
+
+
+class AllSourcesCb extends Checker_cbs;
+   logic [3:0] opcode;
+   logic [2:0] src;
+   const string NAME = "All sources";
+
+   covergroup cg;
+      coverpoint opcode {
+         bins valid_opcodes[] = {
+                                 ADD,
+                                 AND,
+                                 NOT,
+                                 JMP,
+                                 JSR,
+                                 ST,
+                                 STI,
+                                 STR
+                                 };
+         
+      }
+      coverpoint src;
+      cross opcode, src;
+   endgroup
+
+   function new;
+      cg = new();
+      cg.set_inst_name(NAME);
+   endfunction
+
+   task post_tx(ref Transaction tr);
+      opcode = tr.instruction[15:12];
+      unique case (opcode)
+        ADD     : src = tr.instruction[8:6];
+        AND     : src = tr.instruction[8:6];
+        NOT     : src = tr.instruction[8:6];
+        JMP     : src = tr.instruction[8:6];
+        JSR     : src = tr.instruction[8:6];
+        ST      : src = tr.instruction[11:9];
+        STI     : src = tr.instruction[11:9];
+        STR     : src = tr.instruction[8:6];
+        default : src = 3'bXXX; // Let's see some red if we didn't do this right
+      endcase
       cg.sample();
    endtask
 
@@ -95,7 +161,9 @@ class TestRandomGood extends TestBase;
 
    task create_callbacks;
       AllOpcodesCb cb1 = new();
+      AllSourcesCb cb2 = new();
       env.chk.cbs.push_back(cb1);
+      env.chk.cbs.push_back(cb2);
    endtask
 endclass
 
